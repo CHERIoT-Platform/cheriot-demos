ff5914a595c174d3ba7ec213071884aa8f8f0d17
Nathaniel Wesley Filardo <nwfilardo@gmail.com>
2025-02-25 17:46:57 +0000

    frontend: more functionality in grid view

--- smartmeter/frontends/static/grid.js
+++ smartmeter/frontends/static/grid.js
@@ -1,5 +1,31 @@
 import * as democonfig from "./democonfig.mjs";
 
+/////
+// Snoop on the "user" timebase so that "now" evolves in parallel
+/////
+
+var timebaseZero;
+var timebaseRate;
+
+function timebaseRetime(sec)
+{
+  if (timebaseZero === null)
+  {
+    return sec;
+  }
+
+  if (sec >= timebaseZero)
+  {
+    return timebaseZero + timebaseRate * (sec - timebaseZero);
+  }
+
+  return sec;
+}
+
+/////
+// MQTT Client
+/////
+
 const mqttClient = mqtt.connect(
   democonfig.MQTT_BROKER_WSS_URL,
   { clean: true
@@ -8,17 +34,107 @@ const mqttClient = mqtt.connect(
 
 mqttClient.on("message", (t, m) =>
   {
-    console.log("Message", t.toString(), m.toString());
+    const mString = m.toString();
+    console.log("Message", t.toString(), mString);
     if (t.startsWith("cheriot-smartmeter/g/update"))
     {
-      document.getElementById("latest-update").textContent = m.toString();
+      document.getElementById("latest-update").textContent = mString;
+    }
+    else if (t.startsWith("cheriot-smartmeter/u/timebase"))
+    {
+      const newTimebase = /^\s*(\d+)\s+(\d+)\s*$/.exec(mString);
+      timebaseZero = parseInt(newTimebase[1]);
+      timebaseRate = parseInt(newTimebase[2]);
+      console.log(`New timebase: ${timebaseZero} ${timebaseRate}`);
     }
   });
 mqttClient.on("connect", () =>
   {
     console.log("Connected");
-    mqttClient.subscribe("cheriot-smartmeter/g/update/#", (err) =>
-      {
-        if(err) { console.log("Failed to subscribe", err); }
-      });
   });
+
+/////
+// Grid alert form handling
+/////
+
+const gridAlertForm = document.getElementById("gridAlertForm");
+const gridAlertDuration = gridAlertForm.querySelector("#gridAlertDuration");
+const gridAlertSelect = gridAlertForm.querySelector("#gridAlertSelect");
+const gridAlertSubmit = gridAlertForm.querySelector("#gridAlertSubmit");
+
+gridAlertForm.addEventListener("submit", (event) =>
+  {
+    event.preventDefault();
+
+    const now = timebaseRetime(Date.now() / 1000 | 0);
+    const duration = gridAlertDuration.value * 60;
+    const value = gridAlertSelect.value;
+
+    console.log(`Publishing grid alert: ${lastMeterId} ${now} ${duration} ${value}`);
+    if (mqttClient)
+    {
+      mqttClient.publish(`cheriot-smartmeter/g/request/${lastMeterId}`, `${now} ${duration} ${value}`);
+    }
+  });
+
+
+/////
+// Meter ID form handling
+/////
+
+const meterIDForm = document.getElementById("meterIDForm");
+const meterIDValue = meterIDForm.querySelector("#meterIdentity");
+const meterIDSubmit = meterIDForm.querySelector("#meterIDSubmit");
+
+function buttonEnables(needMeterID)
+{
+    meterIDSubmit.disabled = !needMeterID;
+    gridAlertSubmit.disabled = needMeterID;
+}
+
+var lastMeterId; // for unsubscription
+function onNewMeterID(newID)
+{
+    if (mqttClient && lastMeterId !== null)
+    {
+      mqttClient.unsubscribe(`cheriot-smartmeter/g/update/${lastMeterId}`,
+        (err) => { if(err) { console.log("Failed to unsubscribe from updates?", err); } }
+      );
+      mqttClient.unsubscribe(`cheriot-smartmeter/u/timebase/${lastMeterId}`,
+        (err) => { if(err) { console.log("Failed to unsubscribe from timebase?", err); } }
+      );
+    }
+
+    lastMeterId = newID;
+    timebaseZero = null;
+
+    if (mqttClient)
+    {
+      mqttClient.subscribe(`cheriot-smartmeter/g/update/${lastMeterId}`,
+        (err) => { if(err) { console.log("Failed to subscribe?", err); } }
+      );
+      mqttClient.subscribe(`cheriot-smartmeter/u/timebase/${lastMeterId}`,
+        (err) => { if(err) { console.log("Failed to subscribe to timebase?", err); } }
+      );
+
+    }
+
+    buttonEnables(false);
+}
+
+meterIDValue.addEventListener("input", (event) =>
+  {
+    buttonEnables(true);
+  });
+
+meterIDForm.addEventListener("submit", (event) =>
+  {
+    event.preventDefault();
+    onNewMeterID(meterIDValue.value);
+  });
+
+if (democonfig.DEFAULT_METER_ID !== null)
+{
+  meterIDValue.value = democonfig.DEFAULT_METER_ID;
+  onNewMeterID(meterIDValue.value);
+}
