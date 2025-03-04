c7e93f1667f9296da296388566528f2f250f7210
Robert Norton-Wright <robert.norton@scisemi.com>
2025-03-04 16:29:41 +0000

    Add RPi demo js config and add an option to allow self-signed cert.

--- smartmeter/frontends/static/democonfig.mjs
+++ smartmeter/frontends/static/democonfig.mjs
@@ -1,3 +1,1 @@
-export const MQTT_BROKER_MQTTS_URL = "mqtts://test.mosquitto.org:8883";
-export const MQTT_BROKER_WSS_URL = "wss://test.mosquitto.org:8081/x";
-export const DEFAULT_METER_ID = null;
+mosquitto.org-config.mjs
