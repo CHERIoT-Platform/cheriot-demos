c553158ad9e9e8caf627640e9ba4340a4dbc6f7d
Nathaniel Wesley Filardo <nwfilardo@gmail.com>
2025-03-09 21:57:54 +0000

    Tweak default policy
    And make it really the default JS baked in.  While here, make that
    process easier and document it.

--- smartmeter/cheriot/js/compile.sh
+++ smartmeter/cheriot/js/compile.sh
@@ -1,5 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 set -e
+
+unset HEADER_OUT
+
+while getopts "H:" o; do
+	case "$o" in
+		H) HEADER_OUT="${OPTARG}";;
+		*) exit 1;
+	esac
+done
+shift $((OPTIND - 1))
 
 if [ -z "$1" ] ; then
 	echo Usage: $0 {javascript file}
@@ -21,6 +31,30 @@ if [ ! -f ./node_modules/.bin/microvium ] ; then
 	npm install microvium
 fi
 
-./node_modules/.bin/microvium $1
+declare -a MVM_CMD
+MVM_CMD=(./node_modules/.bin/microvium "$1")
 
+[ -n "${HEADER_OUT-}" ] && {
+	MVM_CMD+=("--output-bytes")
+	MVM_CMD+=(">>")
+	MVM_CMD+=("${HEADER_OUT}")
+
+    cat > "${HEADER_OUT}" <<HERE
+/*
+ * Auto-generated file from ${1}; do not edit.
+ * See js/compile.sh.
+ */
+unsigned char hello_mvm_bc[] =
+HERE
+}
+
+eval "${MVM_CMD[@]}"
 echo Output in $1.mvm-bc
+
+[ -n "${HEADER_OUT-}" ] && {
+    cat >> "${HEADER_OUT}" <<HERE
+;
+unsigned int hello_mvm_bc_len = sizeof(hello_mvm_bc);
+HERE
+	clang-format -i "${HEADER_OUT}"
+}
